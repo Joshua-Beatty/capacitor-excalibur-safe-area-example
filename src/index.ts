7ebbd64ec3117505413ed65b4d0d421f1dcda3bc
Joshua Beatty <joshuatb6@gmail.com>
2022-05-21 22:26:53 -0600

    testing

--- src/index.ts
+++ src/index.ts
@@ -62,12 +62,11 @@ game.add(leftBar)
 leftBar.on('preupdate', (evtObj) => {
     const thing = Number(getComputedStyle(document.documentElement).getPropertyValue("--sal").replace("px", "")) / window.innerWidth * game.screen.resolution.width
     leftBar.pos.x =  - game.currentScene.camera.viewport.width / 2 + leftBar.width / 2 + thing
-    console.log('safeAreaInsets.support', safeAreaInsets.support)
-    console.log("left: " + safeAreaInsets.left)
-    console.log("right: " + safeAreaInsets.right)
-    console.log("top: " + safeAreaInsets.top)
-    console.log("bottom: " + safeAreaInsets.bottom)
-    console.log("win: " + window.innerWidth)
+    
+    console.log("sat: " + getComputedStyle(document.documentElement).getPropertyValue("--sat"))
+    console.log("sab: " + getComputedStyle(document.documentElement).getPropertyValue("--sab"))
+    console.log("sal: " + getComputedStyle(document.documentElement).getPropertyValue("--sal"))
+    console.log("sar: " + getComputedStyle(document.documentElement).getPropertyValue("--sar"))
   })
 
 game.input.pointers.primary.on('down', function (evt) {
